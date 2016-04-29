a8b5bd19302ff95693cb84de60d959f3df1a08e4
tanyishu <kaobei_lah@hotmail.com>
2016-04-29 12:09:01 +0800

    add basic authentication

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login Page' });
+});
+
+router.post('/login', function(req, res, next) {
+  if (req.body.username === 'yishu' && req.body.password === 'password') {
+    res.redirect('/')
+  } else {
+    res.redirect('/login')
+  }
+
+});
+
 module.exports = router;
